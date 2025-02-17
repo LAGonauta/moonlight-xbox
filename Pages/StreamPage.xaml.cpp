819f2b97a2714d117b24dece40def81453da5991
LAGonauta <lagonauta@gmail.com>
2025-02-17 00:06:49 -0500

    Fixed multiple crashes due to nullptr MoonlightClient
    Also some refactoring and the usage of weak pointers

--- Pages/StreamPage.xaml.cpp
+++ Pages/StreamPage.xaml.cpp
@@ -66,7 +66,7 @@ void StreamPage::Page_Loaded(Platform::Object^ sender, Windows::UI::Xaml::Routed
 		// At this point we have access to the device. 
 		// We can create the device-dependent resources.
 		m_deviceResources->SetSwapChainPanel(swapChainPanel);
-		m_main = std::unique_ptr<moonlight_xbox_dxMain>(new moonlight_xbox_dxMain(m_deviceResources,this,new MoonlightClient(),configuration));
+		m_main = std::make_unique<moonlight_xbox_dxMain>(m_deviceResources, this, new MoonlightClient(), configuration);
 		m_main->CreateDeviceDependentResources();
 		m_main->CreateWindowSizeDependentResources();
 		m_main->StartRenderLoop();
